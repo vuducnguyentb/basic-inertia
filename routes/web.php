01c1e2e17dc69e2e7f4e151b98f510924fa5ecbd
Vũ Đức Nguyên <nguyenvd@tcv.vn>
2022-06-13 10:12:05 +0700

    update basic inertia

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,9 +16,16 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-   return Inertia::render('Wellcome');
-})->name('home');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
 
-Route::get('/another-page',function(){
-   return Inertia::render('Another');
-})->name('another');
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,11 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .vue()
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+    .postCss('resources/css/app.css', 'public/css', [require('tailwindcss'), require('autoprefixer')])
+    .alias({
+        '@': 'resources/js',
+    });
+
+if (mix.inProduction()) {
+    mix.version();
+}
